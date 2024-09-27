45ec4eb87858159ac21b30d32f9d5d1623478d98
Maxim <rugooglikru@gmail.com>
2024-09-27 21:41:15 +0300

    new

--- sum.cpp
+++ sum.cpp
@@ -7,6 +7,7 @@ int main()
 
 	cout<<"Enter two integers: ";
 	cin >> firstNumber >> secondNumber;
+	sum = firstNumber + secondNumber;
 	cout << firstNumber << " + " << secondNumber << " = " << sum;
 	return 0;
 }
